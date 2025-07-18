fea43204247a8609a29206840ef5f10893ae17fe
Abinaya Rajasekara <156398764+abinaya-232001@users.noreply.github.com>
2025-07-18 13:32:54 +0530

    Update internship_tracker.py

--- internship_tracker.py
+++ internship_tracker.py
@@ -4,7 +4,14 @@ import spacy
 from sklearn.feature_extraction.text import TfidfVectorizer
 from datetime import datetime
 
-nlp = spacy.load("en_core_web_sm")
+import spacy
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    from spacy.cli import download
+    download("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
+
 
 # Page config & styling
 st.set_page_config(page_title="Internship Tracker", layout="wide")
